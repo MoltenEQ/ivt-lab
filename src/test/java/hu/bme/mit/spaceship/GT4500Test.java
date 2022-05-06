2e8fa5226098a8e219f7168e05325f627d813d1f
MoltenEQ <MoltenEQ.official@gmail.com>
2022-05-06 08:58:04 +0200

    Added mock for the tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,54 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primaryTorpedoStoreMock;
+  private TorpedoStore secondaryTorpedoStoreMock;
+
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primaryTorpedoStoreMock =  mock(TorpedoStore.class);
+    secondaryTorpedoStoreMock =  mock(TorpedoStore.class);
+    this.ship = new GT4500(primaryTorpedoStoreMock,secondaryTorpedoStoreMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primaryTorpedoStoreMock.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStoreMock.isEmpty()).thenReturn(false);
+
+    when(primaryTorpedoStoreMock.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStoreMock.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primaryTorpedoStoreMock,times(1)).fire(1);
+    verify(secondaryTorpedoStoreMock,times(0)).fire(1);
+
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primaryTorpedoStoreMock.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStoreMock.isEmpty()).thenReturn(false);
+
+    when(primaryTorpedoStoreMock.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStoreMock.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(primaryTorpedoStoreMock,times(1)).fire(1);
+    verify(secondaryTorpedoStoreMock,times(1)).fire(1);
   }
 
 }
